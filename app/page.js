ab512f3e75802cb44ce2e83b868dc6ab13198c2e
Francis Bello <bellofrancis87@gmail.com>
2023-11-19 21:16:16 +0100

    used dynamic import

--- app/page.js
+++ app/page.js
@@ -1,15 +1,60 @@
+// "use client";
+// import { useEffect, useState } from "react";
+// import Hero from "./components/Hero";
+// import gsap from "gsap/dist/gsap";
+// import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+// import dynamic from "next/dynamic";
+// import FeatureSlides from "./components/FeatureSlides";
+
+// export default function Home() {
+//   const [loader, setLoader] = useState(true);
+
+
+// const DynamicHeader = dynamic(() => import("./components/Hero"), {
+//   ssr: false,
+// });
+//   useEffect(() => {
+//     if (typeof window !== "null") {
+//       setTimeout(() => {
+//         setLoader(false);
+//       }, 500);
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     if (typeof window !== "null") {
+//       gsap.registerPlugin(ScrollTrigger);
+//       console.log(window);
+//     }
+//   }, []);
+
+//   return loader ? (
+//     <div className="loader" />
+//   ) : (
+//     <div>
+//       <Hero />
+//       <FeatureSlides />
+//       <div className="footer">Build your ideal story today.</div>
+//     </div>
+//   );
+// }
+
 "use client";
 import { useEffect, useState } from "react";
-import Hero from "./components/Hero";
+import dynamic from "next/dynamic";
 import gsap from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import FeatureSlides from "./components/FeatureSlides";
+
+const DynamicHeader = dynamic(() => import("./components/Hero"), {
+  ssr: false,
+});
 
 export default function Home() {
   const [loader, setLoader] = useState(true);
 
   useEffect(() => {
-    if (typeof window !== "null") {
+    if (typeof window !== null) {
       setTimeout(() => {
         setLoader(false);
       }, 500);
@@ -17,7 +62,7 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    if (typeof window !== "null") {
+    if (typeof window !== null) {
       gsap.registerPlugin(ScrollTrigger);
       console.log(window);
     }
@@ -27,7 +72,7 @@ export default function Home() {
     <div className="loader" />
   ) : (
     <div>
-      <Hero />
+      <DynamicHeader />
       <FeatureSlides />
       <div className="footer">Build your ideal story today.</div>
     </div>
